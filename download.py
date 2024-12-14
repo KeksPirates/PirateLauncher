865f79b25e8f9ac89ee637feb612a8bf4306ead3
Vxrtrauter <101264710+Vxrtrauter@users.noreply.github.com>
2024-12-14 22:49:06 +0100

    Update download.py

--- download.py
+++ download.py
@@ -1,5 +1,6 @@
 import aria2p
 import subprocess
+import keyboard
 
 aria2 = aria2p.API(
     aria2p.Client(
@@ -9,7 +10,17 @@ aria2 = aria2p.API(
     )
 )
 
-subprocess.Popen(["aria2c", "--enable-rpc", "--rpc-listen-all=true", "--dir=/home/username/Downloads"])
+
+def pauseDownload():
+    print("Download is being paused...")
+    aria2.pause_all
+
+def startDownload():
+    download = aria2.add_magnet(magnet_uri)
+
+
+subprocess.Popen(["aria2c", "--enable-rpc", "--rpc-listen-all=true", "--dir=C:\\Users\\shayaa\\Downloads"])
+
 
 downloads = aria2.get_downloads()
 
@@ -17,4 +28,10 @@ for download in downloads:
     print(download.name, downloads.download_speed, download.status)
     
 magnet_uri = "magnet:?xt=urn:btih:7FBF03CCC9115260DC50102E5A31E83E91422A79&dn=Ready+or+Not%3A+LSPD+Bundle+%28v66884+%2B+4+DLCs%2FBonuses+%2B+Windows+7+Fix%2C+MULTi12%29+%5BFitGirl+Repack%2C+Selective+Download+-+from+34.7+GB%5D&tr=udp%3A%2F%2Fopentor.net%3A6969&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.theoks.net%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.ccp.ovh%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=https%3A%2F%2Ftracker.tamersunion.org%3A443%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.bt4g.com%3A2095%2Fannounce&tr=udp%3A%2F%2Fbt2.archive.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fbt1.archive.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.filemail.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker1.bt.moack.co.kr%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce"
-download = aria2.add_magnet(magnet_uri)
+
+keyboard.on_press_key("d", startDownload, suppress=False)
+keyboard.on_press_key("s", pauseDownload, suppress=False)
+
+
+
+keyboard.wait("q")
