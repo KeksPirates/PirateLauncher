ef7150cf76ef9371a8e451de28ba34906ee2ee6b
KeksNino <timmastercraft123@gmail.com>
2024-12-15 15:13:44 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-<<<<<<< HEAD
 url = "https://steamrip.com/games-list-page/"
 
 response = requests.get(url)
@@ -66,49 +65,3 @@ else:
 # url = "https://collectionchamber.blogspot.com/"
 # url = "https://archive.org/details/classicpcgames"
 # url = "https://websites.umich.edu/~archive/"
-=======
-# DirectDL
-DDL = {
-    "https://steamrip.com/",
-    "https://cs.rin.ru/",
-    "https://gamesdrive.net/",
-    "https://gog-games.to/",
-    "https://forum.torrminatorr.com/",
-    "https://ankergames.net/",
-    "https://www.ovagames.com/",
-    "https://online-fix.me/",
-    "https://gamebounty.world/",
-    "https://steamgg.net/",
-    "https://g4u.to/",
-    "https://appnetica.com/",
-    "https://atopgames.com/",
-    "https://games4u.org/",
-    "https://rexagames.com/",
-    "https://cr4ckpass.online/",
-    "https://getfreegames.net/",
-    "https://gload.to/",
-    "https://steamunderground.net/",
-    "https://worldofpcgames.com/",
-    "http://www.leechinghell.pw/",
-    "https://awtdg.site/",
-    "https://www.cg-gamespc.com/"
-    "https://gamepcfull.com/",
-}
-
-# Repacks/Torrents
-re_tor = {
-    "https://www.kaoskrew.org/",
-    "https://fitgirl-repacks.site/",
-    "https://m4ckd0ge-repacks.site/",
-    "https://byxatab.com/",
-    "https://digital-zone.xyz/",
-    "https://elamigos.site/",
-    "https://dodi-repacks.site/",
-    "https://www.tiny-repacks.win/",
-    "https://freegogpcgames.com/",
-    "https://www.magipack.games/",
-    "https://collectionchamber.blogspot.com/",
-    "https://archive.org/details/classicpcgames",
-    "https://websites.umich.edu/~archive/",
-}
->>>>>>> origin/main
