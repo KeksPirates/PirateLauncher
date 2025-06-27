b34d2e64c1159a03065005035b5c2db736efd7a0
KeksNino <timmastercraft123@gmail.com>
2025-06-27 23:58:45 +0200

    initial gui commit with search field

--- core/gui.py
+++ core/gui.py
@@ -1,1 +1,33 @@
-# shit is emptyyyy
+from PySide6 import QtWidgets
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QLineEdit, QWidget, QVBoxLayout
+import sys
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Software Manager")
+        self.setGeometry(100, 100, 800, 600)
+
+        self.controls = QWidget()
+        self.controlsLayout = QVBoxLayout()
+
+        # Searchbar
+        self.searchbar = QLineEdit()
+
+        container = QWidget()
+        containerLayout = QVBoxLayout()
+        containerLayout.addWidget(self.searchbar)
+
+        container.setLayout(containerLayout)
+        self.setCentralWidget(container)
+
+        containerLayout.setAlignment(Qt.AlignmentFlag.AlignTop)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MainWindow()
+    widget.show()
+
+    sys.exit(app.exec())
