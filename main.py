440651d363e3d617d297af3437fb03f134ad31c4
Vxrtrauter <gmakberh@gmail.com>
2025-06-28 03:46:56 +0200

    Run Gui from Main.py

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-import core.gui as gui
-from core.uztracker_scraper import scrape_uztracker, get_post_title, get_magnet_link
 from core.gui import run_gui
 
-
-
-
-
-
-
-
-
-
-
-post_url = scrape_uztracker()
-if post_url:
-    maintitle = get_post_title(post_url)
-    magnetlink = get_magnet_link(post_url)
-    print(maintitle)
-    print(magnetlink)
-
-
-
+run_gui()
