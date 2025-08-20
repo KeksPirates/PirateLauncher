ebdedb253645aa426502db6b71d9b471fe546a17
KeksNino <KeksNino@proton.me>
2025-08-20 17:37:50 +0200

    readded print selected software function
    and also returns the selected software

--- core/gui.py
+++ core/gui.py
@@ -36,12 +36,27 @@ class MainWindow(QtWidgets.QMainWindow, QWidget):
         self.softwareList.addItems(searchresults)
 
         containerLayout.addWidget(self.button)
-        self.button.clicked.connect(lambda: function())
+        self.button.clicked.connect(lambda: self.download_selected())
+        self.button.clicked.connect(lambda: self.get_selected_item())
 
         container.setLayout(containerLayout)
         self.setCentralWidget(container)
 
         containerLayout.setAlignment(Qt.AlignmentFlag.AlignBottom)
+
+    def download_selected(self):
+        item = self.softwareList.currentItem()
+        if item is not None:
+            print(f"Downloading {self.softwareList.currentItem().text()}")
+        else:
+            print("No item selected for download.")
+
+
+    def get_selected_item(self):
+        item = self.softwareList.currentItem()
+        if item is not None:
+            return item.text()
+        return ""
 
     def return_pressed(self):
         search_text = self.searchbar.text()
@@ -51,7 +66,6 @@ class MainWindow(QtWidgets.QMainWindow, QWidget):
             self.softwareList.addItems(self.postnames)
         print("User searched for:", search_text)
 
-    
 
 
 
