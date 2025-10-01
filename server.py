39bd4953077e0e89739e5a20ff7530f77cf16b52
Vxrtrauter <gmakberh@gmail.com>
2025-10-02 01:16:09 +0200

    Add error handling for empty search queries

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import jsonify
 from scraping.rutracker import scrape_rutracker
 from scraping.rutracker import init
 
@@ -15,6 +16,11 @@ init()
 @app.route('/search/<search_term>')
 def search(search_term):
     return scrape_rutracker(search_term)    
+
+@app.route("/search/")
+def search_empty():
+    return jsonify(error="no search query defined"), 400
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
