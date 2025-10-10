b0596e9710b6a9313b8022bd855c16949c718436
KeksNino <KeksNino@proton.me>
2025-10-10 02:41:49 +0200

    create config file. doesnt load yet

--- core/utils/settings.py
+++ core/utils/settings.py
@@ -1,5 +1,6 @@
 from core.utils.data.state import state
-
+import os
+import platform
 
 def restart_aria2c():
     import main # had to do this because of circle import :(
@@ -13,7 +14,19 @@ def restart_aria2c():
     atexit.register(main.kill_aria2server)
 
 def save_settings(thread_count, close, apiurl):
+    if platform.system() == "Windows":
+        config_dir = os.environ.get("APPDATA", os.path.expanduser("~\\AppData\\Roaming"))
+    else:
+        config_dir = os.environ.get("XDG_CONFIG_HOME", os.path.expanduser("~/.config"))
+    settings_path = os.path.join(config_dir, "SoftwareManager")
+    os.makedirs(settings_path, exist_ok=True)
+
+    with open(os.path.join(settings_path, "config.yml"), 'w') as f:
+        f.write(f"aria2_threads: {state.aria2_threads}\n")
+        f.write(f"api_url: {state.api_url}\n")
+
     state.aria2_threads = thread_count
     state.api_url = apiurl
     restart_aria2c()
+
     close()
