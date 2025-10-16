5af9e79ecc36414cbf71b67f392b947526b0c107
Vxrtrauter <gmakberh@gmail.com>
2025-10-16 22:55:52 +0200

    Refractor searchhelper to finally be readable, reverd uztracker scraper to old format since i didn't like it

--- core/interface/utils/searchhelper.py
+++ core/interface/utils/searchhelper.py
@@ -5,29 +5,41 @@ from core.utils.network.jsonhandler import split_data, format_data
 from core.utils.data.state import state
 
 def return_pressed(self):
-        search_text = self.searchbar.text()
-        if search_text == "":
-            if state.debug:
-                print("Error: Can't search for nothing")
-            return
+    search_text = self.searchbar.text()
+    if search_text == "":
         if state.debug:
-            print("User searched for:", search_text)
-        if state.tracker == "uztracker":
-            response = asyncio.run(scrape_uztracker(search_text))
-        if state.tracker == "rutracker":
-            response = scrape_rutracker(search_text)
+            print("Error: Can't search for nothing")
+        return
+    if state.debug:
+        print("User searched for:", search_text)
+    
+    if state.tracker == "uztracker":
+        response = scrape_uztracker(search_text)
         if response:
-            if state.tracker == "uztracker":
-                state.post_titles, state.post_urls, = response
+            state.post_titles, state.post_urls = response
+            self.softwareList.clear()
+            if state.post_titles and len(state.post_titles) > 0:
+                self.softwareList.addItems(state.post_titles)
+                self.show_empty_results(False)
+                self.post_author_list.clear()
+            else:
+                if state.debug:
+                    print(f"No Results for {search_text}")
                 self.softwareList.clear()
-                if state.post_titles:
-                    self.softwareList.addItems(state.post_titles)
-                    self.post_author_list.addItems(state.post_author)
-            if state.tracker == "rutracker":
-                _, state.posts, _, _, cached = split_data(response)
+                self.show_empty_results(True)
+        else:
+            if state.debug:
+                print(f"No response from uztracker")
+            self.softwareList.clear()
+            self.show_empty_results(True)
+    
+    elif state.tracker == "rutracker":
+        response = scrape_rutracker(search_text)
+        if response:
+            _, state.posts, _, _, cached = split_data(response)
             if state.posts == []:
                 if state.debug:
-                    print(f"No Results found for \"{search_text}\"")
+                    print(f"No Results for {search_text}")
                 self.softwareList.clear()
                 self.show_empty_results(True)
             else:
@@ -39,4 +51,8 @@ def return_pressed(self):
                 self.softwareList.addItems(state.post_titles)
                 if state.debug == True:
                     print(f"Response Cached: {cached}")
-
+        else:
+            if state.debug:
+                print(f"No response from rutracker")
+            self.softwareList.clear()
+            self.show_empty_results(True)
