5637e8065703f226761f80d18b33e93694a5b0fc
Vxrtrauter <gmakberh@gmail.com>
2025-10-17 21:23:17 +0200

    added some more testing stuff

--- core/data/scrapers/monkrus.py
+++ core/data/scrapers/monkrus.py
@@ -1,18 +1,26 @@
+from core.utils.data.state import state, Post
 from bs4 import BeautifulSoup
 import requests
 
 
 
 def scrape_monkrus_telegram():
-    telegram_channel = "https://t.me/s/real_monkrus/rss"
+    telegram_channel = "https://t.me/s/real_monkrus/"
+    posts: list[Post] = []
 
     response = requests.get(telegram_channel)
     print(response.status_code) # rm later
 
-    soup = BeautifulSoup(response.text, 'html.parser')
-    posts_txt = soup.find_all('div', class_=["tgme_widget_message_wrap js-widget_message_wrap", "tgme_widget_message_wrap js-widget_message_wrap date_visible"])
+    soup = BeautifulSoup(response.text, "html.parser")
+    posts = soup.find_all("div", class_=["tgme_widget_message_wrap js-widget_message_wrap", "tgme_widget_message_wrap js-widget_message_wrap date_visible"])
+
+    for post in posts:
+        posts_txt = post.find("div", class_="tgme_widget_message_text js-message_text") # this doesnt work
+
+
 
     print(posts_txt)
+
 
 
 scrape_monkrus_telegram()
--- core/utils/data/state.py
+++ core/utils/data/state.py
@@ -20,3 +20,10 @@ class AppState:
     aria2_threads: int = 4
 
 state = AppState(posts=[], post_titles=[], post_urls=[], post_author=[], downloads=[], download_path="")
+
+@dataclass
+class Post:
+    id: int
+    title: str
+    url: str
+    author: str
